0446d3a3fd6e1a1c4f3236d853a1c2a0b960c1c1
James Dibble <james@jdibble.co.uk>
2021-05-03 21:10:52 +0100

    Add project files.

---
+++ src/DashTransit.App/Program.cs
@@ -0,0 +1,24 @@
+// <copyright file="Program.cs" company="James Dibble">
+// Copyright (c) James Dibble. All rights reserved.
+// </copyright>
+
+namespace DashTransit.App
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ src/DashTransit.App/Startup.cs
@@ -0,0 +1,48 @@
+// <copyright file="Startup.cs" company="James Dibble">
+// Copyright (c) James Dibble. All rights reserved.
+// </copyright>
+
+namespace DashTransit.App
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using MudBlazor.Services;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddMudServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
