f16abdafa059efd8c99c1408c669a383b1a061d7
James Dibble <>
2021-12-09 13:37:05 +0000

    Sort the layout out ish

--- src/DashTransit.EntityFramework/Repository.cs
+++ src/DashTransit.EntityFramework/Repository.cs
@@ -1,6 +1,7 @@
 namespace DashTransit.EntityFramework;
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Ardalis.Specification;
@@ -21,35 +22,24 @@ public class Repository<T> : RepositoryBase<T>, IReadRepositoryBase<IRawAuditDat
     }
 
     public Task<int> CountAsync(ISpecification<IRawAuditData> specification, CancellationToken cancellationToken = default)
-    {
-        throw new System.NotImplementedException();
-    }
+        => SpecificationEvaluator.Default.GetQuery(this.context.Set<RawAudit>(), specification).CountAsync(cancellationToken);
 
     public Task<TResult> GetBySpecAsync<TResult>(ISpecification<IRawAuditData, TResult> specification, CancellationToken cancellationToken = default)
-    {
-        throw new System.NotImplementedException();
-    }
+        => SpecificationEvaluator.Default.GetQuery(this.context.Set<RawAudit>(), specification).FirstOrDefaultAsync(cancellationToken);
 
     public Task<List<IRawAuditData>> ListAsync(ISpecification<IRawAuditData> specification, CancellationToken cancellationToken = default)
-    {
-        throw new System.NotImplementedException();
-    }
+        => SpecificationEvaluator.Default.GetQuery(this.context.Set<RawAudit>(), specification).ToListAsync(cancellationToken);
+    
 
     public Task<List<TResult>> ListAsync<TResult>(ISpecification<IRawAuditData, TResult> specification, CancellationToken cancellationToken = default)
         => SpecificationEvaluator.Default.GetQuery(this.context.Set<RawAudit>(), specification).ToListAsync(cancellationToken);
 
-    Task<IRawAuditData> IReadRepositoryBase<IRawAuditData>.GetByIdAsync<TId>(TId id, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+    async Task<IRawAuditData> IReadRepositoryBase<IRawAuditData>.GetByIdAsync<TId>(TId id, CancellationToken cancellationToken)
+        => await this.context.Set<RawAudit>().FindAsync(new object[] { id }, cancellationToken: cancellationToken);
 
     Task<IRawAuditData> IReadRepositoryBase<IRawAuditData>.GetBySpecAsync<Spec>(Spec specification, CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+        => SpecificationEvaluator.Default.GetQuery(this.context.Set<RawAudit>(), specification).FirstOrDefaultAsync(cancellationToken);
 
-    Task<List<IRawAuditData>> IReadRepositoryBase<IRawAuditData>.ListAsync(CancellationToken cancellationToken)
-    {
-        throw new System.NotImplementedException();
-    }
+    async Task<List<IRawAuditData>> IReadRepositoryBase<IRawAuditData>.ListAsync(CancellationToken cancellationToken)
+        => (await this.context.Set<RawAudit>().ToListAsync(cancellationToken)).Cast<IRawAuditData>().ToList();
 }
