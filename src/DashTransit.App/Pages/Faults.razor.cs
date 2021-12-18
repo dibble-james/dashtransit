6e01cf6559b7c00343ac441b8c4427f75982bb41
James Dibble <>
2021-12-18 11:33:10 +0000

    Start to add faults

---
+++ src/DashTransit.App/Pages/Faults.razor.cs
@@ -0,0 +1,48 @@
+namespace DashTransit.App.Pages;
+
+using DashTransit.Core.Application.Queries;
+using Fluxor;
+using MediatR;
+
+public partial class Faults
+{
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+        this.Dispatcher.Dispatch(new FetchData());
+    }
+
+    [ReducerMethod(typeof(FetchData))]
+    public static MessagesState OnFetch(MessagesState state) => state with { Loading = true };
+
+    [ReducerMethod]
+    public static MessagesState OnFetched(MessagesState state, Fetched action) => state with { Loading = false, Messages = action.Messages.ToList() };
+
+    [FeatureState]
+    public record MessagesState
+    {
+        public bool Loading { get; init; } = true;
+        public List<LatestMessages> Messages { get; init; } = new List<LatestMessages>();
+    }
+
+    public class Handlers
+    {
+        private readonly IMediator mediator;
+
+        public Handlers(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [EffectMethod(typeof(FetchData))]
+        public async Task HandleFetchDataAction(IDispatcher dispatcher)
+        {
+            var messages = await this.mediator.Send(new LatestMessagesQuery());
+            dispatcher.Dispatch(new Fetched(messages));
+        }
+    }
+
+    public record FetchData();
+
+    public record Fetched(IEnumerable<LatestMessages> Messages);
+}
