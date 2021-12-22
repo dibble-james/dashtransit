a1a16a7cc64b9d4a3d5b77b250c846fcaca385cb
James Dibble <>
2021-12-22 09:52:08 +0000

    Load messages in conversation

---
+++ src/DashTransit.App/Components/Actors.razor.cs
@@ -0,0 +1,70 @@
+// <copyright file="Actors.razor.cs" company="James Dibble">
+// Copyright (c) James Dibble. All rights reserved.
+// </copyright>
+
+namespace DashTransit.App.Components;
+
+using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Models;
+using Core.Application.Queries;
+using Core.Domain;
+using Fluxor;
+using MediatR;
+
+public partial class Actors
+{
+    private void LoadActors()
+    {
+        this.Dispatcher.Dispatch(new Fetch(this.MessageId));
+    }
+
+    [ReducerMethod(typeof(Fetch))]
+    public static ActorState OnFetch(ActorState state) => state with { Loading = true };
+
+    [ReducerMethod]
+    public static ActorState OnFetched(ActorState state, Fetched action) => state with { Loading = false, Actors = action.Actors };
+
+
+    public class Handlers
+    {
+        private readonly IMediator mediator;
+
+        public Handlers(IMediator mediator) => this.mediator = mediator;
+
+        [EffectMethod]
+        public async Task HandleFetchActorsAction(Fetch action, IDispatcher dispatcher)
+        {
+            var actors = await this.mediator.Send(new MessageActors(action.Id));
+            var diagram = new Diagram();
+
+            var initiator = actors.First(x => x is Sender or Publisher);
+            var initiatorNode = new NodeModel { Title = initiator.Endpoint.ToString() };
+            initiatorNode.AddPort(PortAlignment.Right);
+            diagram.Nodes.Add(initiatorNode);
+
+            foreach (var consumer in actors.Where(x => x is Consumer))
+            {
+                var node = new NodeModel { Title = consumer.Endpoint.ToString() };
+                node.AddPort(PortAlignment.Left);
+
+                diagram.Nodes.Add(node);
+                diagram.Links.Add(new LinkModel(initiatorNode.GetPort(PortAlignment.Right), node.GetPort(PortAlignment.Left)));
+            }
+
+            diagram.AutoArrange();
+
+            dispatcher.Dispatch(new Fetched(diagram));
+        }
+    }
+
+    [FeatureState]
+    public record ActorState
+    {
+        public bool Loading { get; init; } = true;
+        public Diagram? Actors { get; init; }
+    }
+
+    public record Fetch(MessageId Id);
+
+    public record Fetched(Diagram Actors);
+}
--- src/DashTransit.App/Pages/Message.razor.cs
+++ src/DashTransit.App/Pages/Message.razor.cs
@@ -15,31 +15,17 @@ public partial class Message
         this.Dispatcher.Dispatch(new FetchData(Core.Domain.AuditId.From(this.AuditId)));
     }
 
-    private void LoadActors()
-    {
-        this.Dispatcher.Dispatch(new FetchActors(MessageId.From(this.State.Value.Message!.MessageId!.Value)));
-    }
-
     [ReducerMethod(typeof(FetchData))]
     public static MessageState OnFetch(MessageState state) => state with { Loading = true };
 
     [ReducerMethod]
     public static MessageState OnFetched(MessageState state, Fetched action) => state with { Loading = false, Message = action.Message };
 
-    [ReducerMethod(typeof(FetchActors))]
-    public static MessageState OnFetchActors(MessageState state) => state with { LoadingActors = true, Actors = null };
-
-    [ReducerMethod]
-    public static MessageState OnActorsFetched(MessageState state, ActorsFetched action) => state with { ActorsLoaded = true, LoadingActors = false, Actors = action.Actors };
-
     [FeatureState]
     public record MessageState
     {
         public bool Loading { get; init; } = true;
         public IRawAuditData? Message { get; init; }
-        public bool LoadingActors { get; init; } = true;
-        public bool ActorsLoaded { get; init; } = false;
-        public Diagram? Actors { get; init; }
     }
 
     public class Handlers
@@ -54,38 +40,9 @@ public partial class Message
             var message = await this.mediator.Send(new MessageByAuditId(action.AuditId));
             dispatcher.Dispatch(new Fetched(message));
         }
-
-        [EffectMethod]
-        public async Task HandleFetchActorsAction(FetchActors action, IDispatcher dispatcher)
-        {
-            var actors = await this.mediator.Send(new MessageActors(action.Id));
-            var diagram = new Diagram();
-
-            var initiator = actors.First(x => x is Sender or Publisher);
-            var initiatorNode = new NodeModel { Title = initiator.Endpoint.ToString() };
-            initiatorNode.AddPort(PortAlignment.Right);
-            diagram.Nodes.Add(initiatorNode);
-
-            foreach (var consumer in actors.Where(x => x is Consumer))
-            {
-                var node = new NodeModel { Title = consumer.Endpoint.ToString() };
-                node.AddPort(PortAlignment.Left);
-
-                diagram.Nodes.Add(node);
-                diagram.Links.Add(new LinkModel(initiatorNode.GetPort(PortAlignment.Right), node.GetPort(PortAlignment.Left)));
-            }
-
-            diagram.AutoArrange();
-
-            dispatcher.Dispatch(new ActorsFetched(diagram));
-        }
     }
 
     public record FetchData(AuditId AuditId);
 
     public record Fetched(IRawAuditData? Message);
-
-    public record FetchActors(MessageId Id);
-
-    public record ActorsFetched(Diagram Actors);
 }
