4998929637396aa7ff29dacb23639f7c977c605b
James Dibble <dibble-james@github.com>
2022-01-01 20:08:42 +0000

    Add argument for migrations

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 Gain an insight into your MassTransit installation
 
 [![CI Build](https://github.com/dibble-james/dashtransit/actions/workflows/ci.yml/badge.svg?branch=trunk)](https://github.com/dibble-james/dashtransit/actions/workflows/ci.yml)
+
 ## Features
+
 - View your audit logs
 - See related messages
 - See the endpoints associated with a message
@@ -11,28 +13,35 @@ Gain an insight into your MassTransit installation
 - Re-send failed messages
 
 ### Coming soon...
+
 - Search audit logs
 - Send new message
 - Edit and re-send
 
 ## Supported Configurations
+
 More to follow but for now we support:
 
 | Transport |
-| -- |
-| RabbitMQ |
+| --------- |
+| RabbitMQ  |
 
-| Storage |
-| -- |
+| Storage    |
+| ---------- |
 | SQL Server |
 
 ## Getting started
+
 DashTransit is distributed as a Docker container.
 
-If you're using storage that requires a schema, run the migrations for the appropriate database engine.
+You can create the tables DashTransit requires by migrating using:
+
+```
+docker run --name dashtransit-migrations -e store__connection= ghcr.io/dibble-james/dashtransit migrate
+```
 
 Just provide connection strings for the storage and transport. The web app is exposed on port 80.
 
 ```
-docker run -d -p 80:<desired port> -n dashtransit -e connectionstrings__transport= -e connectionstrings__storage= ghcr.io/dibble-james/dashtransit
+docker run -d -p 80:<desired port> --name dashtransit -e transport__connection= -e store_connection= ghcr.io/dibble-james/dashtransit
 ```
--- src/DashTransit.App/Program.cs
+++ src/DashTransit.App/Program.cs
@@ -2,6 +2,7 @@ using DashTransit.Core;
 using DashTransit.EntityFramework;
 using Fluxor;
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,19 +12,37 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddFluxor(opt => opt.ScanAssemblies(typeof(Program).Assembly));
 
 builder.Services.AddDashTransit();
-builder.Services.UseDashTransitEntityFramework(builder.Configuration.GetConnectionString("store"));
+builder.Services.UseDashTransitEntityFramework(builder.Configuration.GetValue<string>("store:connection"));
 builder.Services.AddMassTransitHostedService();
 builder.Services.AddMassTransit(bus =>
 {
     bus.AddDashTransit();
     bus.UsingRabbitMq((context, rabbit) =>
     {
-        rabbit.Host(builder.Configuration.GetConnectionString("transport"));
+        rabbit.Host(builder.Configuration.GetValue<string>("transport:connection"));
         rabbit.UseDashTransit(context);
     });
 });
 
 var app = builder.Build();
+
+if (args.FirstOrDefault()?.Equals("migrate", StringComparison.InvariantCultureIgnoreCase) == true)
+{
+    using var scope = app.Services.CreateScope();
+    var database = scope.ServiceProvider.GetRequiredService<DashTransitContext>().Database;
+    Console.Out.WriteLine($"Applying migrations for DashTransit");
+    try
+    {
+        database.Migrate();
+        Console.Out.WriteLine("Migrations applied successfully");
+    }
+    catch (Exception ex)
+    {
+        Console.Error.WriteLine($"Appliying migrations failed:\r\n{ex.Message}");
+    }
+    return;
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
